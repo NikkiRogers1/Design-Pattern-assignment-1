165caff44a61150230fae1ecd928441c65c241ed
NikkiRogers1 <nikkiharris1@student.bpcc.edu>
2026-08-21 12:52:28 -0500

    Complete phase 2 Strategy Pattern

--- FlyingHero.java
+++
@@ -1,9 +0,0 @@
-public class FlyingHero extends Hero {
-    public FlyingHero(String name) {
-    super(name);
-    }   
-    @Override   
-    public void move() {
-        System.out.println("I am soaring through the clouds!");
-    }
-}
--- SwimmingHero.java
+++
@@ -1,9 +0,0 @@
-public class SwimmingHero extends Hero {
-    public SwimmingHero(String name) {
-        super(name);
-    }
-    @Override
-    public void move() {
-        System.out.println( "I am gliding through the water.");
-    }
-}
